ec8c059491a458d98c87f60008163b82c8cb49d1
Brandy Knust <brandy_1175@hotmail.com>
2021-04-06 18:03:48 -0500

    4-6

---
+++ app.py
@@ -0,0 +1,38 @@
+# 1. imports
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# 2. Database setup
+engine = create_engine("sqlite:///..Resources/hawaii.sqlite")
+
+# 3. reflect an existing database into a new model
+Base = automap_base()
+# and reflect the tables
+Base.prepare(engine, reflect=True) 
+# and save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# 4. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+# 5. Define name, location, and email
+name = "Climate"
+location = "Houston"
+email = rice@mail.com
+
+# 6. Define routes
+@app.route("/")
+def index():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )return
